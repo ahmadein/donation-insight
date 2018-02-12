86b41f1b9c017fbca373ef40feffca5b6f8e88a3
Ahmed <ahmedin@ucdavis.edu>
2018-02-12 01:30:35 -0800

    readme added, runtime calculated, memory size calculated

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -1,5 +1,8 @@
 import numpy as np
 import argparse
+import time
+import sys
+start_time = time.time()
 # parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-in", "--input_file", metavar="FILE NAME", required=False, default='../input/itcont.txt', help="enter input file path")
@@ -11,7 +14,7 @@ filename = args.input_file
 percentile_filename = args.percentile_file
 out_file_name = args.out_file
 columns_header = {'CMTE_ID': 0, 'NAME': 7, 'ZIP_CODE': 10, 'TRANSACTION_DT': 13, 'TRANSACTION_AMT': 14, 'OTHER_ID': 15}  # default dictionary for which columns to read
-df_index_to_keep = [columns_header['CMTE_ID'], columns_header['NAME'], columns_header['ZIP_CODE'], columns_header['TRANSACTION_DT'], columns_header['TRANSACTION_AMT'], columns_header['OTHER_ID']]
+index_to_keep = [columns_header['CMTE_ID'], columns_header['NAME'], columns_header['ZIP_CODE'], columns_header['TRANSACTION_DT'], columns_header['TRANSACTION_AMT'], columns_header['OTHER_ID']]
 # order of processing by default 0,7,10,13,14,15
 cmte_id_dict = {}
 donor_list = {}
@@ -30,7 +33,7 @@ with open(filename) as f:
             or len(data_txt[columns_header['ZIP_CODE']]) < 5 or data_txt[columns_header['TRANSACTION_DT']] == '' or
                 data_txt[columns_header['TRANSACTION_AMT']] == '' or data_txt[columns_header['OTHER_ID']] != ''):
             continue  # if line is invalid or other ID has value, get the next line
-        filtered_data = [data_txt[i] for i in df_index_to_keep]
+        filtered_data = [data_txt[i] for i in index_to_keep]
         filtered_data[2] = filtered_data[2][:5]  # first 5 charachters of zip code
         filtered_data[4] = round(float(filtered_data[4]))
         zip_code = filtered_data[2]
@@ -56,3 +59,6 @@ with open(filename) as f:
             foutobj.write('|'.join(str(i) for i in data_to_write)+'\n')
 foutobj.close()
 print('done with number of line ' + str(line_number))
+print("--- %s seconds ---" % (time.time() - start_time))
+print('sizes of dicts')
+print(sys.getsizeof(uniq_donor_id), sys.getsizeof(cmte_id_dict))
